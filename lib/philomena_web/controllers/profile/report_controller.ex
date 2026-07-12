e862edf54afe9a8b1a9ac1bf3dbe2057691fa756
Luna Fox <luna@nighty.cloud>
2026-07-12 11:59:20 +0200

    context logic - profile report controller

--- lib/philomena_web/controllers/profile/report_controller.ex
+++ lib/philomena_web/controllers/profile/report_controller.ex
@@ -1,46 +1,60 @@
 defmodule PhilomenaWeb.Profile.ReportController do
   use PhilomenaWeb, :controller
 
-  alias PhilomenaWeb.ReportController
   alias PhilomenaWeb.ReportView
-  alias Philomena.Users.User
-  alias Philomena.Reports.Report
   alias Philomena.Reports
 
-  plug PhilomenaWeb.FilterBannedUsersPlug
   plug PhilomenaWeb.UserAttributionPlug
   plug PhilomenaWeb.CaptchaPlug
   plug PhilomenaWeb.CheckCaptchaPlug when action in [:create]
-  plug PhilomenaWeb.CanaryMapPlug, new: :show, create: :show
 
-  plug :load_and_authorize_resource,
-    model: User,
-    id_name: "profile_id",
-    id_field: "slug",
-    persisted: true
+  action_fallback PhilomenaWeb.FallbackController
 
-  def new(conn, _params) do
-    user = conn.assigns.user
-    action = ~p"/profiles/#{user}/reports"
+  def new(conn, %{"profile_id" => slug}) do
+    with {:ok, {user, changeset}} <- Reports.load_user_for_report(conn.assigns.actor, slug) do
+      action = ~p"/profiles/#{user}/reports"
 
-    changeset =
-      %Report{reportable_type: "User", reportable_id: user.id}
-      |> Reports.change_report()
-
-    conn
-    |> put_view(ReportView)
-    |> render("new.html",
-      title: "Reporting User",
-      reportable: user,
-      changeset: changeset,
-      action: action
-    )
+      conn
+      |> put_view(ReportView)
+      |> render("new.html",
+        title: "Reporting User",
+        reportable: user,
+        changeset: changeset,
+        action: action
+      )
+    end
   end
 
-  def create(conn, params) do
-    user = conn.assigns.user
-    action = ~p"/profiles/#{user}/reports"
+  def create(conn, %{"profile_id" => slug} = params) do
+    with {:ok, user} <- Reports.load_user_for_report_creation(conn.assigns.actor, slug) do
+      action = ~p"/profiles/#{user}/reports"
 
-    ReportController.create(conn, action, "User", user, params)
+      case Reports.create_report(conn.assigns.actor, "User", user.id, params["report"]) do
+        {:ok, _report} ->
+          conn
+          |> put_flash(
+            :info,
+            "Your report has been received and will be checked by staff shortly."
+          )
+          |> redirect(to: report_redirect_path(conn.assigns.current_user))
+
+        {:error, :too_many_reports} ->
+          conn
+          |> put_flash(
+            :error,
+            "You may not have more than #{Reports.max_open_reports()} open reports at a time. " <>
+              "Did you read the reporting tips?"
+          )
+          |> redirect(to: "/")
+
+        {:error, %Ecto.Changeset{} = changeset} ->
+          conn
+          |> put_view(ReportView)
+          |> render("new.html", reportable: user, changeset: changeset, action: action)
+      end
+    end
   end
+
+  defp report_redirect_path(nil), do: "/"
+  defp report_redirect_path(_user), do: ~p"/reports"
 end
