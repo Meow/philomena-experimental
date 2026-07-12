66de9b26f68962231965042c675091b84243444f
Luna Fox <luna@nighty.cloud>
2026-07-12 11:59:24 +0200

    context logic - profile commission report controller

--- lib/philomena_web/controllers/profile/commission/report_controller.ex
+++ lib/philomena_web/controllers/profile/commission/report_controller.ex
@@ -1,66 +1,67 @@
 defmodule PhilomenaWeb.Profile.Commission.ReportController do
   use PhilomenaWeb, :controller
 
-  alias PhilomenaWeb.ReportController
   alias PhilomenaWeb.ReportView
-  alias Philomena.Users.User
-  alias Philomena.Reports.Report
   alias Philomena.Reports
 
-  plug PhilomenaWeb.FilterBannedUsersPlug
   plug PhilomenaWeb.UserAttributionPlug
   plug PhilomenaWeb.CaptchaPlug
   plug PhilomenaWeb.CheckCaptchaPlug when action in [:create]
-  plug PhilomenaWeb.CanaryMapPlug, new: :show, create: :show
 
-  plug :load_resource,
-    model: User,
-    id_name: "profile_id",
-    id_field: "slug",
-    preload: [
-      :verified_links,
-      commission: [
-        sheet_image: [:sources, tags: :aliases],
-        user: [awards: :badge],
-        items: [example_image: [:sources, tags: :aliases]]
-      ]
-    ],
-    persisted: true
+  action_fallback PhilomenaWeb.FallbackController
 
-  plug :ensure_commission
+  def new(conn, %{"profile_id" => slug}) do
+    with {:ok, {user, commission, changeset}} <-
+           Reports.load_commission_for_report(conn.assigns.actor, slug) do
+      action = ~p"/profiles/#{user}/commission/reports"
 
-  def new(conn, _params) do
-    user = conn.assigns.user
-    commission = conn.assigns.user.commission
-    action = ~p"/profiles/#{user}/commission/reports"
-
-    changeset =
-      %Report{reportable_type: "Commission", reportable_id: commission.id}
-      |> Reports.change_report()
-
-    conn
-    |> put_view(ReportView)
-    |> render("new.html",
-      title: "Reporting Commission",
-      reportable: commission,
-      changeset: changeset,
-      action: action
-    )
-  end
-
-  def create(conn, params) do
-    user = conn.assigns.user
-    commission = conn.assigns.user.commission
-    action = ~p"/profiles/#{user}/commission/reports"
-
-    ReportController.create(conn, action, "Commission", commission, params)
-  end
-
-  defp ensure_commission(conn, _opts) do
-    if is_nil(conn.assigns.user.commission) do
-      PhilomenaWeb.NotFoundPlug.call(conn)
-    else
       conn
+      |> put_view(ReportView)
+      |> render("new.html",
+        title: "Reporting Commission",
+        reportable: commission,
+        changeset: changeset,
+        action: action
+      )
     end
   end
+
+  def create(conn, %{"profile_id" => slug} = params) do
+    with {:ok, {user, commission}} <-
+           Reports.load_commission_for_report_creation(conn.assigns.actor, slug) do
+      action = ~p"/profiles/#{user}/commission/reports"
+
+      case Reports.create_report(
+             conn.assigns.actor,
+             "Commission",
+             commission.id,
+             params["report"]
+           ) do
+        {:ok, _report} ->
+          conn
+          |> put_flash(
+            :info,
+            "Your report has been received and will be checked by staff shortly."
+          )
+          |> redirect(to: report_redirect_path(conn.assigns.current_user))
+
+        {:error, :too_many_reports} ->
+          conn
+          |> put_flash(
+            :error,
+            "You may not have more than #{Reports.max_open_reports()} open reports at a time. " <>
+              "Did you read the reporting tips?"
+          )
+          |> redirect(to: "/")
+
+        {:error, %Ecto.Changeset{} = changeset} ->
+          conn
+          |> put_view(ReportView)
+          |> render("new.html", reportable: commission, changeset: changeset, action: action)
+      end
+    end
+  end
+
+  defp report_redirect_path(nil), do: "/"
+  defp report_redirect_path(_user), do: ~p"/reports"
 end
